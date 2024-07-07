5e7c1e98fdf939e73443120536edb9fbd3d78157
Ovy Evbodi <evbodiovo@gmail.com>
2024-07-07 17:13:35 +0100

    Adjust home section 2

--- components/BigCard.tsx
+++ components/BigCard.tsx
@@ -6,12 +6,12 @@ import Image from "next/image";
 
 const BigCard: NextPage<BigCardProps> = ({title, description, image, label}) => {
   return (
-    <div className="md:w-[320px] w-[280px] sm:w-[300px] lg:w-[340px] xl:w-[400px]">
-      <div className="bg-green rounded-[30px] w-full overflow-hidden h-[60%]">
-        <Image src={image} width={400} height={300} alt={label} />
+    <div className="md:w-[320px] w-[255px] sm:w-[280px] lg:w-[300px] xl:w-[340px]">
+      <div className="bg-green rounded-[30px] w-full overflow-hidden h-[350px]">
+        <Image src={image} width={400} height={400} alt={label} />
       </div>
       <div className="p-2 md:p-4 text-leftmd:w-[320px] w-[280px] sm:w-[300px] lg:w-[340px] xl:w-[400px] lg:min-h-[20vh]">
-        <h3 className="text-[1.4rem] text-grey-text leading-8">{title}</h3>
+        <h3 className="text-[1.4rem] text-grey-text font-bold leading-8">{title}</h3>
         <p className="leading-[25px] text-grey-text font-[500]">{description}</p>
         <SmallButton text='Shop now' />
       </div>
