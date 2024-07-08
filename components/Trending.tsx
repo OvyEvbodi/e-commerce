6c9d13af738c560521a41fcf9b32b00bbdd7412c
Ovy Evbodi <evbodiovo@gmail.com>
2024-07-08 18:17:57 +0100

    Fix layout on home page

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -3,13 +3,16 @@
 import SmallButton from "@/components/SmallButton";
 import {trending_cards} from "@/api/data.json";
 import TrendingCard from "@/components/TrendingCard";
+import Link from "next/link";
 
 const Trending = () => {
   return (
     <div id="trending" className="bg-off-white lg:min-h-[100vh] w-[100vw]">
       <div className="flex flex-wrap justify-between items-center p-4 lg:py-6 lg:px-12">
         <h3 className="font-bold leading-8 text-[1.5rem]">Trending products for you </h3>
-        <SmallButton text="View all products" flag="fill" />
+        <Link href="/products">
+          <SmallButton text="View all products" flag="fill" />
+        </Link>
       </div>
       <div className="min-h-[80vh] p-4 lg:py-6 lg:px-12 flex flex-wrap justify-evenly items-stretch gap-4 lg:gap-6">
         {
--- components/TrendingCard.tsx
+++ components/TrendingCard.tsx
@@ -6,10 +6,9 @@ import Image from "next/image";
 
 const TrendingCard: NextPage<BigCardProps> = ({title, image, label, price}) => {
   return (
-    <div className="relative w-[250px] sm:w-[280px] md:w-[300px] xl:w-[320px]">
-      <div className="product_card rounded-[6px] border border-[3px] border-green w-full overflow-hidden h-[320px]">
-        <Image src={image} width={350} height={600} alt={label} />
-        {/* <img src={image} className="h-full w-full"/> */}
+    <div className="w-[250px] sm:w-[280px] md:w-[300px] xl:w-[330px]">
+      <div className="rounded-[6px] w-full overflow-hidden h-[321px]">
+        <Image src={image} width={1753} height={1709} alt={label} />
       </div>
       <div className="p-2 md:p-4 text-left w-full lg:min-h-[20vh]">
         <h5 className="text-[1.05rem] text-grey-text leading-8">{title}</h5>
