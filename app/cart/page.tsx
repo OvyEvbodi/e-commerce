fe0b465acd32b27581baef99ae68d3d30bc38ed4
Ovy Evbodi <evbodiovo@gmail.com>
2024-07-09 10:59:36 +0100

    Optimize cart for responsiveness

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -22,7 +22,7 @@ const CartPage = () => {
         <div  className="md:min-w-[700px] lg:w-2/3  p-4 lg:px-12">
           <div className="bg-grey-bg rounded-[8px] text-[1.2rem] flex flex-wrap justify-between items-center p-2">
             <h6 className="font-[500] md:font-bold">Item</h6>
-            <div className="md:font-[600] lg:font-bold md:w-3/5 text-[1.15rem] md:text-[1.3rem] gap-4 flex flex-wrap justify-evenly items-center p-2">
+            <div className="hidden md:flex md:font-[600] lg:font-bold md:w-3/5 text-[1rem] sm:text-[1.15rem] md:text-[1.3rem] gap-4 flex flex-wrap justify-evenly items-center p-2">
               <h6>Price</h6>
               <h6>Quantity</h6>
               <h6>Subtotal</h6>
