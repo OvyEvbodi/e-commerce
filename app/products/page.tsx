b06266ae0f8490f0261c010f6187d870b5874af2
Ovy Evbodi <evbodiovo@gmail.com>
2024-07-09 14:59:42 +0100

    Implement filter dropdown

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -9,10 +9,10 @@ export const metadata = { title: 'Products Helendo' };
 const ProductsPage = () => {
   return (
     <main className="flex flex-wrap">
-      <div className="bg-slate-100 p-4 min-w-[100vw] lg:min-w-[26vw] lg:w-[26vw] lg:min-h-[100vh]">
+      <div className="p-4 min-w-[100vw] lg:min-w-[28vw] lg:w-[28vw] lg:min-h-[100vh]">
         <Filter />
       </div>
-      <div className="p-4 min-w-[100vw] lg:min-w-[74vw] lg:w-[74vw] lg:min-h-[100vh]">
+      <div className="p-4 min-w-[100vw] lg:min-w-[72vw] lg:w-[72vw] lg:min-h-[100vh]">
         <h4 className="text-[1.05rem] font-[500]"></h4>
         <div className="flex flex-wrap gap-4 lg:gap-6 justify-center">
           {
--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -15,8 +15,9 @@ const DropdownOption:NextPage<DropdownOptionProps> = ({ icon, text }) => {
     <div>
       { icon == "yes" ? 
         (
-          <div className="bg-slate-300 p-1 ">
-            <span>0 </span>
+          <div className="flex p-2 bg-grey-bg rounded-[33px] mb-4 font-[600] lg:text-[1.05rem]">
+            <p className="bg-[#becbce] w-[15.64px] h-[15.64px] rounded-full mr-2"></p>
+            <span className="text-orange box-border text-xs border border-orange rounded-full py-[3px] px-[7px] mr-2">&#9210;</span>
             <span>{text}</span>
           </div>
         ) 
@@ -32,9 +33,11 @@ const DropdownOption:NextPage<DropdownOptionProps> = ({ icon, text }) => {
 
 const Dropdown:NextPage<DropdownOptionProps> = ({ text, icon, filters }) => {
   return (
-    <div>
-      <div className="font-bold text-[1.05rem]">{text}</div>
-      <div>
+    <div className="mb-4 md:mb-6 lg:mb-8 py-4 lg:py-6 lg:px-7 border rounded-[20px] border-2 border-black">
+      <div className="font-bold text-[1.05rem] flex justify-between">{text}
+        <span className="text-orange text-sm">&#9206;</span>
+      </div>
+      <div className="">
         {
           filters!.map( (option, index) => (
             <DropdownOption text={option} icon={icon} key={index}/>
