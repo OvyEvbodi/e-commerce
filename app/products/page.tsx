a97f815501c0a065e4fa36c19614353f7dbc8cfb
Ovy Evbodi <evbodiovo@gmail.com>
2024-07-12 21:12:05 +0100

    Add products api route

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,14 +1,19 @@
 // product listing page
 
-import { product_cards } from "@/api/data.json";
+// import { product_cards } from "@/api/data.json";
 import ProductCard from "@/components/productCard";
 import Filter from "@/app/products/Filter";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import BigCardProps from "@/interfaces/BigCardProps";
+import { GET } from "@/app/products/api/route";
 
 export const metadata = { title: 'Products Helendo' };
 
-const ProductsPage = () => {
+
+const ProductsPage = async () => {
+  const productList = await GET();
+  console.log( productList )
 
   return (
     <main className="flex justify-center items-center">
@@ -19,15 +24,16 @@ const ProductsPage = () => {
           </div>
           <div className="p-4 md:pt-10 min-w-[100vw] lg:min-w-[72%] lg:w-[72%] lg:min-h-[100vh]">
             <h4 className="text-[1.05rem] font-[500]"></h4>
-            <div className="flex flex-wrap gap-4 lg:gap-6 justify-center">
+            <p>{JSON.stringify(productList)}</p>
+            {/* <div className="flex flex-wrap gap-4 lg:gap-6 justify-center">
               {
-                product_cards.map(item => (
+                productList.map((item: BigCardProps ) => (
                   <div key={item.id}>
                     <ProductCard { ...item } />
                   </div>
                 ))
               }
-            </div>
+            </div> */}
           </div>
           <ToastContainer />
         </div>
