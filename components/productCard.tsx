59a99a60621e7229362ecb507ec423c660f8ff1e
Ovy Evbodi <evbodiovo@gmail.com>
2024-07-13 12:08:15 +0100

    Link products to their corresponding dynmaic pages

--- components/productCard.tsx
+++ components/productCard.tsx
@@ -10,6 +10,7 @@ import Image from "next/image";
 import { Slide, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { RootState } from "@/redux/store";
+import Link from "next/link";
 
 
 const ProductCard: NextPage<productCardProps> = ({id, title, image, label, price}) => {
@@ -55,11 +56,15 @@ const ProductCard: NextPage<productCardProps> = ({id, title, image, label, price
   
   return (
     <div className="w-[240px] md:w-[280px] lg:w-[320px] xl:w-[350px]">
-      <div className="overflow-hidden ">
-        <Image src={displayImage} width={350} height={600} alt={label} />
-      </div>
+      <Link href={`products/${id}`} >
+        <div className="overflow-hidden ">
+          <Image src={displayImage} width={350} height={600} alt={label} />
+        </div>
+      </Link>
       <div className="py-2 md:py-4 text-left]">
-        <h5 className="text-[1.4rem] text-grey-text font-[400] leading-8">{title}</h5>
+        <Link href={`products/${id}`} >
+          <h5 className="text-[1.4rem] text-grey-text font-[400] leading-8">{title}</h5>
+        </Link>
         <div className="flex">
           <span className="font-bold text-[1.4rem]">${price}</span>
           <span className="text-[0.7rem] line-through">${price}</span>
