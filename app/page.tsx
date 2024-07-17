8c4be640164c04261a6a48c6322401de733803e3
Ovy Evbodi <evbodiovo@gmail.com>
2024-07-17 21:51:54 +0100

    Link homepage back

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 // Homepage
-'use client'
 
 import Hero from "@/components/Hero";
 import HomeSection from "@/components/HomeSection2";
@@ -7,33 +6,19 @@ import Stats from "@/components/Stats";
 import Categories from "@/components/Categories";
 import Newsletter from "@/components/Newsletter";
 import Trending from "@/components/Trending";
-import { useRouter } from 'next/navigation';
-import { useEffect } from "react";
 
-
-// export const metadata = { title: 'Helendo Home' };
-
-// 
-// uncomment the metadata, take out the use client directive,
-// and take out the useEffect hook import and call, take out userouter import
-// 8 lines out, 1 line in
-// uncomment home page jsx
+export const metadata = { title: 'Helendo Home' };
 
 export default function Home() {
-  const router = useRouter();
-  useEffect( () => {
-    router.push('/products')
-  }, []);
-  
 
   return (
     <main>
-      {/* <Hero />
+      <Hero />
       <HomeSection />
       <Stats />
       <Categories />
       <Trending />
-      <Newsletter /> */}
+      <Newsletter />
     </main>
   );
 }
